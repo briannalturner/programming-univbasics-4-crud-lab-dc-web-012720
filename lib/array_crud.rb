2f9621743cfe67012da15abc5dba5057c32a30ae
Bri Turner <blturner3527@gmail.com>
2020-01-09 02:52:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array_length = array.length - 1
+  array[array_length]
 end
 
 def update_element_from_index(array, index_number, element)
